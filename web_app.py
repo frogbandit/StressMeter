86f566ec22f5ed82d4b1085ff2dbe487dc76a541
Steven Lu <tacticalazn@gmail.com>
2013-11-09 04:20:22 +0000

    Adding in sample CRUD

--- web_app.py
+++ web_app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
-import socket
+from pymongo import MongoClient
+
+mongo = MongoClient()
+db = mongo.temp_store
 
 app = Flask(__name__)
 
@@ -7,10 +10,23 @@ app = Flask(__name__)
 def index():
    return render_template("home.html")
 
+@app.route("/add/<name>")
+def add_name(name):
+   db.people.insert({
+      'name': name
+   })
+   return "Your name is %s" % name
+
+@app.route("/remove/<name>")
+def remove_name(name):
+   db.people.remove({'name': name})
+   return "removed"
+
+@app.route("/list")
+def list_names():
+   people = db.people.find()
+   return render_template('people.html', people=people)
+
 if __name__ == "__main__":
-   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   sock.bind(('localhost', 0))
-   port = sock.getsockname()[1]
-   sock.close()
-   app.run(debug=True, host='0.0.0.0', port=port)
+   app.run(debug=True, host='0.0.0.0', port=5000)
 
